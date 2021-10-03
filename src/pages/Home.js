2213600bc21c02306f2d8ad4505612616553265a
fajjarnr <zfajart@gmail.com>
2021-10-03 16:31:52 +0700

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,51 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Jumbotron from "../components/cards/Jumbotron";
+import LoadingCard from "../components/cards/LoadingCard";
+import ProductCard from "../components/cards/ProductCard";
+import { getProductsByCount } from "../functions/product";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(100)
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <div className="jumbotron text-danger h1 font-weight-bold text-center">
+        <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
+      </div>
+
+      <div className="container">
+        {loading ? (
+          <LoadingCard count={3} />
+        ) : (
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4">
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
